774aa2ca55a5e37818a5492d7a8eea8447ea1d20
Razmoket <julien.alaphilippe@gmail.com>
2014-10-01 15:22:22 +0200

    dev

--- FrySurvey/src/fr/fryscop/probe/test/rdds/RddsProbe.java
+++ FrySurvey/src/fr/fryscop/probe/test/rdds/RddsProbe.java
@@ -1,12 +1,28 @@
 package fr.fryscop.probe.test.rdds;
 
-public class RddsProbe {
+import fr.fryscop.probe.test.ProbeTest;
 
-	/* 
-	 * RDDS --> 10 sondes world wide / toutes les 5 min
-	 *  RDDS availability				<--- pas d'erreur lors de requete RDDS si non début du compteur jusqu'à disparition erreur
-	 *  RDDS query RTT					<--- requete simple sur ndd avec mesure du temps entre l'envoi et la réponse
-	 *  RDDS update time					<--- temps entre acquittement d'une modification EPP et la prise en compte effective dans rdds
+public class RddsProbe implements ProbeTest {
+
+	/*
+	 * RDDS --> 10 sondes world wide / toutes les 5 min 
+	 *  RDDS availability <--- pas d'erreur lors de requete RDDS si non début du compteur jusqu'à disparition erreur 
+	 *  RDDS query RTT <--- requete simple sur ndd avec mesure du temps entre l'envoi et la réponse 
+	 *  RDDS update time <--- temps entre acquittement d'une modification EPP et la prise en compte effective dans rdds 
 	 * ==> si RTT 5 fois supérieur au temps SLR alors service unavailable pour la sonde
 	 */
+	
+	@Override
+    public void launchTest() {
+	    // TODO Auto-generated method stub
+	    
+    }
+
+	@Override
+    public String getTestResult() {
+	    // TODO Auto-generated method stub
+	    return null;
+    }
+
+	
 }
