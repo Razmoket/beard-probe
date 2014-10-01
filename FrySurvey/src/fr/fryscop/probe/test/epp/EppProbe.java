5017b8958e60ad9000154820f9e707a0f7e639ea
Razmoket <julien.alaphilippe@gmail.com>
2014-10-01 11:24:18 +0200

    ajout de la description des algo

--- FrySurvey/src/fr/fryscop/probe/test/epp/EppProbe.java
+++ FrySurvey/src/fr/fryscop/probe/test/epp/EppProbe.java
@@ -2,4 +2,12 @@ package fr.fryscop.probe.test.epp;
 
 public class EppProbe {
 
+	/*
+	 * EPP	--> 5 sondes / toutes les 5 min
+	 * 	EPP service availability		<---
+	 * 	EPP session-command RTT
+	 * 	EPP query-command RTT
+	 * 	EPP transform-command RTT
+	 * ==> si RTT 5 fois supérieur au temps SLR alors service unavailable pour la sonde
+	 */
 }
--- FrySurvey/src/fr/fryscop/probe/test/rdds/RddsProbe.java
+++ FrySurvey/src/fr/fryscop/probe/test/rdds/RddsProbe.java
@@ -2,4 +2,11 @@ package fr.fryscop.probe.test.rdds;
 
 public class RddsProbe {
 
+	/* 
+	 * RDDS --> 10 sondes world wide / toutes les 5 min
+	 *  RDDS availability				<--- pas d'erreur lors de requete RDDS si non début du compteur jusqu'à disparition erreur
+	 *  RDDS query RTT					<--- requete simple sur ndd avec mesure du temps entre l'envoi et la réponse
+	 *  RDDS update time					<--- temps entre acquittement d'une modification EPP et la prise en compte effective dans rdds
+	 * ==> si RTT 5 fois supérieur au temps SLR alors service unavailable pour la sonde
+	 */
 }
