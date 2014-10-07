97b9ab946be289fa1ffafa0e8c7b15e27e4ba28e
Razmoket <julien.alaphilippe@gmail.com>
2014-10-07 20:41:06 +0200

    dev dnsprobe

--- FrySurvey/src/fr/fryscop/probe/test/dns/DnsProbe.java
+++ FrySurvey/src/fr/fryscop/probe/test/dns/DnsProbe.java
@@ -1,37 +1,61 @@
 package fr.fryscop.probe.test.dns;
 
+import java.io.IOException;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import fr.fryscop.probe.Probe;
+import fr.fryscop.probe.ProbeType;
 import fr.fryscop.probe.configuration.log.ProbeLogger;
 import fr.fryscop.probe.test.ProbeTest;
 
 /*
- * DNS --> 20 sondes proches des resolvers / every minutes DNS service availability <--- pas d'erreur sur un dig sur un ndd de la zone si non
- * début du compteur jusqu'à disparition erreur DNS name server availability <--- même principe que précédent mais sur les serveur frontaux TCP
- * DNS resolution RTT <--- mesure du temps de la requete DNS en TCP = temps "query time" fournit par dig, + ajouter option "+tcp" UDP DNS
- * resolution RTT <--- mesure du temps de la requete DNS en UDP = temps "query time" fournit par dig DNS update time <--- temps entre acquittement
- * d'une modification EPP et la prise en compte effective dans le dns (valider les données visible dans le dns à modifier) ==> si RTT 5 fois
- * supérieur au temps SLR alors service unavailable pour la sonde
+ * DNS --> 20 sondes proches des resolvers / every minutes 
+ *   DNS service availability <--- pas d'erreur sur un dig sur un ndd de la zone si non début du compteur jusqu'à disparition erreur 
+ *   DNS name server availability <--- même principe que précédent mais sur les serveur frontaux 
+ *   TCP DNS resolution RTT <--- mesure du temps de la requete DNS en TCP = temps "query time" fournit par dig, + ajouter option "+tcp" 
+ *   UDP DNS resolution RTT <--- mesure du temps de la requete DNS en UDP = temps "query time" fournit par dig 
+ *   DNS update time <--- temps entre acquittement d'une modification EPP et la prise en compte effective dans le dns (valider les données visible dans le dns à modifier) 
+ *  ==> si RTT 5 fois supérieur au temps SLR alors service unavailable pour la sonde
  */
-public class DnsProbe implements ProbeTest {
+public class DnsProbe extends Probe implements ProbeTest {
 
-	private static final Logger logger = LoggerFactory.getLogger(DnsProbe.class);
-	@Override
-    public void launchTest() {
-	    // TODO Auto-generated method stub
-	    
-    }
+	private static final Logger	logger	= LoggerFactory.getLogger(DnsProbe.class);
 
 	@Override
-    public String getTestResult() {
-	    // TODO Auto-generated method stub
-		
+	public void launchTest() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public String getTestResult() {
+		// TODO Auto-generated method stub
+
 		ProbeLogger.getInstance();
-	    return null;
-    }
+		return null;
+	}
 
-	
-	
-	
+	void dnsServiceAvailability() {
+
+	}
+
+	void dnsNameServerAvailability() throws IOException {
+		/* dig @g.ext.nic.fr afnic.fr ANY +short +tcp */
+		String argDig[] = { "@g.ext.nic.fr", "afnic.fr", "ANY", "+short", "+tcp" };
+		long requestTime = Dig.request(argDig);
+		System.out.println("dnsNameServerAvailability - requestTime:" + requestTime);
+
+	}
+
+	public static void main(String arg[]) throws IOException {
+		DnsProbe probe = new DnsProbe();
+		probe.setName("afnic");
+		probe.setTld("fr");
+		probe.setType(ProbeType.Dns);
+		
+		probe.dnsNameServerAvailability();
+	}
+
 }
