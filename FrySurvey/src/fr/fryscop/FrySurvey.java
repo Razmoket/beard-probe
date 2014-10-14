28c88378f73fe86abc7d81c6910e35bd3569e12b
Razmoket <julien.alaphilippe@gmail.com>
2014-10-14 12:05:55 +0200

    dev configuration

--- FrySurvey/src/fr/fryscop/FrySurvey.java
+++ FrySurvey/src/fr/fryscop/FrySurvey.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import fr.fryscop.network.protocole.epp.SurveyConnection;
+import fr.fryscop.probe.configuration.Configuration;
 
 public class FrySurvey {
 
@@ -16,17 +17,25 @@ public class FrySurvey {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.setProperty("ssl.props.location", ".\\ssl");
+		logger.info("* * * * * * * * * * * * * * * * * *");
+		logger.info("* * * * * * Démarrage * * * * * * *");
+		logger.info("* * * * * * * * * * * * * * * * * *");
+		/*System.setProperty("ssl.props.location", ".\\ssl");
 		System.setProperty("rtk.props.file",".\\conf\\rtk.properties");
 		logger.info("ssl.props.location=" + System.getProperty("ssl.props.location"));
 		SurveyConnection surveyConnection = new SurveyConnection(args);
-		surveyConnection.session();
+		surveyConnection.session();*/
 		/* * * * * * * * * * * * * * * * * * 
 		 * 192.134.5.70 700 pouet pouet
 		 * 10.4.3.1 6666 -hinpizyz334-.ne 9qPv6MVYX2h1sudR
 		 * 
 		 * * * * * * * * * * * * * * * * * */
-
+		try {
+	        Configuration.loadConfiguration();
+        } catch (Exception e) {
+	        e.printStackTrace();
+        }
+		
 	}
 
 }
--- FrySurvey/src/fr/fryscop/probe/configuration/Configuration.java
+++ FrySurvey/src/fr/fryscop/probe/configuration/Configuration.java
@@ -1,5 +1,42 @@
 package fr.fryscop.probe.configuration;
 
+import java.io.IOException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
+
+import fr.fryscop.probe.test.TestLauncher;
+import fr.fryscop.probe.test.dns.DnsProbe;
+
 public class Configuration {
 
+	private static final Logger logger = LoggerFactory.getLogger(Configuration.class);
+
+	private final static String CONFIGURATION_FILE_NAME= "launcher.tld.xml"; 
+	
+	public static TestLauncher loadConfiguration() throws Exception{
+		//ApplicationContext context = new ClassPathXmlApplicationContext(CONFIGURATION_FILE_NAME);
+		
+		ClassPathResource classPathResource = null;
+        try {
+        	logger.debug("load " + CONFIGURATION_FILE_NAME);
+            classPathResource = new ClassPathResource(CONFIGURATION_FILE_NAME);
+            XmlBeanFactory beanFactory = new XmlBeanFactory(classPathResource);
+            
+            TestLauncher launcher = beanFactory.getBean(TestLauncher.class);
+            return launcher;
+        } catch (Exception e) {
+            String fileName = "";
+            if (classPathResource != null) {
+                try {
+                    fileName = "[" + classPathResource.getFile().getAbsolutePath() + "]";
+                } catch (IOException e1) {
+                    fileName = "[" + CONFIGURATION_FILE_NAME + " not found" + "]";
+                }
+            }
+            throw new Exception("loadConfiguration() failed " + fileName, e);
+        }
+	}
 }
