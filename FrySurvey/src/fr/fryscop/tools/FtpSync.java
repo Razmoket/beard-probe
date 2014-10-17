85d5e6cbe1c7c40f19f591dab6af1faa929933bf
Razmoket <julien.alaphilippe@gmail.com>
2014-10-17 22:13:52 +0200

    dev client ftp

---
+++ FrySurvey/src/fr/fryscop/tools/FtpSync.java
@@ -0,0 +1,109 @@
+package fr.fryscop.tools;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.InetAddress;
+
+import org.apache.commons.net.ftp.FTP;
+import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.ftp.FTPReply;
+import org.apache.commons.net.io.CopyStreamEvent;
+import org.apache.commons.net.io.CopyStreamListener;
+
+public class FtpSync {
+
+	public static void main(String args[]) {
+		FTPClient ftp = new FTPClient();
+		//	FTPClientConfig config = new FTPClientConfig();
+		//config.setXXX(YYY); // change required options
+		ftp.setControlKeepAliveTimeout(300);
+		//ftp.configure(config );
+		boolean error = false;
+
+		/*
+		 * Serveur FTP             : ftp.frysurvey.fr ou ftp.cluster007.ovh.net
+		 * Utilisateur (login)     : frysurvewg
+		 * Mot de passe (password) : Ym5YE9SeCDvZ
+		 */
+		String server = "ftp.frysurvey.fr";
+		String username = "frysurvewg";
+		String password = "Ym5YE9SeCDvZ";
+		try {
+			int reply;
+			ftp.connect(server);
+			ftp.login(username, password);
+			System.out.println("Connected to " + server + ".");
+			System.out.print(ftp.getReplyString());
+
+			// After connection attempt, you should check the reply code to verify
+			// success.
+			reply = ftp.getReplyCode();
+
+			if (!FTPReply.isPositiveCompletion(reply)) {
+				ftp.disconnect();
+				System.err.println("FTP server refused connection.");
+				System.exit(1);
+			}
+			// do the job there
+			System.out.println("Remote system is " + ftp.getSystemType());
+
+			ftp.setFileType(FTP.BINARY_FILE_TYPE, FTP.BINARY_FILE_TYPE);
+	        ftp.setFileTransferMode(FTP.BINARY_FILE_TYPE);
+	        
+			ftp.enterLocalPassiveMode();
+			ftp.setCopyStreamListener(createListener());
+		
+			String localhostName = InetAddress.getLocalHost().getHostName();
+			String local = "archive-log-probe/probe-log-file.20141015.log.zip";
+			String remote = "/log_probes/" + localhostName + "_probe-log-file.20141015.log.zip";
+
+			InputStream input;
+			input = new FileInputStream(local);
+
+			System.out.println("local:"+local);
+			System.out.println("remote:"+remote);
+			boolean storeFile = ftp.storeFile(remote, input);
+			System.out.println("storeFile:" + storeFile);
+			reply = ftp.getReplyCode();
+			System.out.println("reply:"+reply);
+			input.close();
+
+			
+			// end job
+			ftp.logout();
+		} catch (IOException e) {
+			error = true;
+			e.printStackTrace();
+		} finally {
+			if (ftp.isConnected()) {
+				try {
+					ftp.disconnect();
+				} catch (IOException ioe) {
+					// do nothing
+				}
+			}
+			System.exit(error ? 1 : 0);
+		}
+	}
+
+	private static CopyStreamListener createListener() {
+		return new CopyStreamListener() {
+			private long	megsTotal	= 0;
+
+			//            @Override
+			public void bytesTransferred(CopyStreamEvent event) {
+				bytesTransferred(event.getTotalBytesTransferred(), event.getBytesTransferred(), event.getStreamSize());
+			}
+
+			//            @Override
+			public void bytesTransferred(long totalBytesTransferred, int bytesTransferred, long streamSize) {
+				long megs = totalBytesTransferred / 1000000;
+				for (long l = megsTotal; l < megs; l++) {
+					System.err.print("#");
+				}
+				megsTotal = megs;
+			}
+		};
+	}
+}
