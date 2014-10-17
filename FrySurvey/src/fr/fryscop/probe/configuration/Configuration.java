9a6487416e9a9fb4d260f498fc1eebdaceaef350
Razmoket <julien.alaphilippe@gmail.com>
2014-10-17 23:27:43 +0200

    dev

--- FrySurvey/src/fr/fryscop/probe/configuration/Configuration.java
+++ FrySurvey/src/fr/fryscop/probe/configuration/Configuration.java
@@ -8,12 +8,14 @@ import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.core.io.ClassPathResource;
 
 import fr.fryscop.probe.test.TestLauncher;
+import fr.fryscop.tools.FolderSpyWorker;
 
 public class Configuration {
 
-	private static final Logger logger = LoggerFactory.getLogger(Configuration.class);
+	private static final Logger	logger	                = LoggerFactory.getLogger(Configuration.class);
 
-	private final static String CONFIGURATION_FILE_NAME = "probes-launcher.xml";
+	private final static String	CONFIGURATION_FILE_NAME	= "probes-launcher.xml";
+	private final static String	CONFIGURATION_FTP_FILE_NAME	= "ftp_sync.xml";
 
 	public static TestLauncher loadConfiguration() throws Exception {
 		logger.info("load " + CONFIGURATION_FILE_NAME);
@@ -36,6 +38,30 @@ public class Configuration {
 				}
 			}
 			throw new Exception("loadConfiguration() failed " + fileName, e);
-		}	
+		}
+	}
+
+	public static FolderSpyWorker loadFtpConfiguration() throws Exception {
+		logger.info("load " + CONFIGURATION_FTP_FILE_NAME);
+		// ApplicationContext context = new ClassPathXmlApplicationContext(CONFIGURATION_FILE_NAME);
+
+		ClassPathResource classPathResource = null;
+		try {
+			classPathResource = new ClassPathResource(CONFIGURATION_FTP_FILE_NAME);
+			XmlBeanFactory beanFactory = new XmlBeanFactory(classPathResource);
+
+			FolderSpyWorker launcher = beanFactory.getBean(FolderSpyWorker.class);
+			return launcher;
+		} catch (Exception e) {
+			String fileName = "";
+			if (classPathResource != null) {
+				try {
+					fileName = "[" + classPathResource.getFile().getAbsolutePath() + "]";
+				} catch (IOException e1) {
+					fileName = "[" + CONFIGURATION_FTP_FILE_NAME + " not found" + "]";
+				}
+			}
+			throw new Exception("loadConfiguration() failed " + fileName, e);
+		}
 	}
 }
